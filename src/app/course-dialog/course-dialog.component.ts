4a4259bd5681b184f20ac7f41af910d5f2eaf64e
kokoyroy <kokoyroy@gmail.com>
2021-02-27 14:07:50 +0200

    reqs hell

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import { fromEvent } from 'rxjs';
+import { concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/internal-compatibility';
 
 @Component({
     selector: 'course-dialog',
@@ -15,29 +15,45 @@ import {fromPromise} from 'rxjs/internal-compatibility';
 export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     form: FormGroup;
-    course:Course;
+    course: Course;
 
     @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+    @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+        @Inject(MAT_DIALOG_DATA) course: Course) {
 
         this.course = course;
 
-        this.form = fb.group({
+        this.form = this.fb.group({
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
             releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
+            longDescription: [course.longDescription, Validators.required]
         });
 
     }
 
     ngOnInit() {
+        this.form.valueChanges
+            .pipe(filter(() => this.form.valid))
+            .subscribe(changes => {
+               const saveCourse$ = fromPromise(fetch(`/api/courses/${this.course.id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify(changes),
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+
+                }))
+                saveCourse$.subscribe()
+            })
+        // this.form.valueChanges.subscribe(console.log)
+        // console.log(this.form);
+
 
 
 
